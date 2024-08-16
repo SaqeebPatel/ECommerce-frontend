6b540f11931aa63f66f1841724e346d997554ec4
SaqeebPatel <saqeeb3p@gmail.com>
2024-08-16 19:45:49 +0530

    Add a new code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,86 @@
-import React from 'react'
+// import React from 'react'
+
+// const Navbar = () => {
+//   return (
+//     <div>
+//       <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//   <div className="container-fluid">
+    
+//     <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+//         <li className="nav-item">
+//           <a className="nav-link active" aria-current="page" href="#">Home</a>
+//         </li>
+//        <li className="nav-item dropdown">
+//           <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+//             Dropdown
+//           </a>
+//           <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+//             <li><a className="dropdown-item" href="#">Action</a></li>
+//             <li><a className="dropdown-item" href="#">Another action</a></li>
+//             <li><hr className="dropdown-divider"/></li>
+//             <li><a className="dropdown-item" href="#">Something else here</a></li>
+//           </ul>
+//         </li>
+       
+//       </ul>
+//       <form className="d-flex">
+//         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
+//         <button className="btn btn-outline-success" type="submit">Search</button>
+//       </form>
+//     </div>
+//   </div>
+// </nav>
+//     </div>
+
+//   )
+// }
+
+// export default Navbar
+import React from 'react';
 
 const Navbar = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-       <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-       
-      </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
+        <div className="container-fluid">
+          
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="d-flex align-items-center ms-3">
+              <img 
+                src="https://previews.123rf.com/images/mamun25g/mamun25g2007/mamun25g200700784/151666606-sp-s-p-letter-logo-design-initial-letter-sp-linked-circle-uppercase-monogram-logo-red-and-blue-s-p.jpg" 
+                alt="Profile" 
+                className="rounded-circle" 
+                width="40" 
+                height="40" 
+              />
+            </div>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+              <li className="nav-item">
+                <a className="nav-link active ml-3" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a className="dropdown-item" href="#">Action</a></li>
+                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            
+          </div>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav>
-    </div>
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
